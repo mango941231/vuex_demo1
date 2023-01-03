7b0175a04fc8a3fe05b7f76e4bd8033892da526d
mango <1119197389@qq.com>
2023-01-03 16:09:35 +0800

    上传

--- src/components/Addition.vue
+++ src/components/Addition.vue
@@ -1,10 +1,16 @@
 <template>
+  <div>
+    <h3>当前最新的count值为：</h3>
+    <button>+1</button>
+  </div>
 
 </template>
 
 <script>
 export default {
-  name: 'Addition'
+  data() {
+    return {};
+  }
 }
 </script>
 
